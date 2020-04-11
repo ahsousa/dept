6699e3109b92177a0ec4f7bee5c3e4efec9b3bb0
Andre Sousa <andrehend.sousa@gmail.com>
2020-04-11 13:39:32 +0100

    package update

--- src/app/core/components/header-nav/header-nav.component.ts
+++ src/app/core/components/header-nav/header-nav.component.ts
@@ -7,7 +7,7 @@ import { GlobalsService } from '../../services/globals.service';
   styleUrls: ['./header-nav.component.scss']
 })
 export class HeaderNavComponent implements OnInit {
-
+  menuOpened: boolean;
   imageFormat: string;
   supportsWebP: boolean;
 
@@ -17,5 +17,12 @@ export class HeaderNavComponent implements OnInit {
     this.globals.getBrowser() === 'Chrome' || this.globals.getBrowser() === 'Firefox' ?
       this.supportsWebP = true :
       this.supportsWebP = false;
+    this.menuOpened = false;
+
+  }
+
+  openMenu(): void {
+    console.log(this.menuOpened);
+    this.menuOpened = !this.menuOpened;
   }
 }
